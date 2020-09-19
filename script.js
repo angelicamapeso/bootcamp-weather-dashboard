82a7636e7007f13cf84c1fb01f58bc0b6e94ea11
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-09-19 16:57:40 -0400

    Add comments for JavaScript

---
+++ script.js
@@ -0,0 +1,36 @@
+//handle user input
+  //does not submit if user tries to submit nothing
+
+//fetch the data
+  //error if city could not be found
+  
+  //if city can be found and data retrieved
+  //display city in search history 
+    //Ensure that first letter is capitalized
+  //display data in overview card
+  //display data in 5 day forecast
+  
+
+  //save search term in object (only if success!)
+    //properties:
+      //current
+      //search history[]
+
+//on page load
+  //grab the search object from local storage
+  //display the overview info from current (make another fetch)
+  //display search history
+  //display 5 day forecast
+
+  //if none
+    //center text- enter city to see weather
+
+//when city clicked in search history
+  //if selection isn't current
+    //make another fetch
+    //display search history
+    //display overview card
+    //display 5 day forecast
+
+
+
