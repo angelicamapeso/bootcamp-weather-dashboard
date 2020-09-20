993411dcb26aff2c1cdc473124a948c6e7bfc504
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-09-20 02:10:07 -0400

    Reorganized functions and added comments

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //Search button event listener
 document.getElementById('search-city').addEventListener('submit', submitSearch);
 
+/***** Handling user input *****/
+//when search submitted
 function submitSearch(event) {
   event.preventDefault();
   resetSearchError();
@@ -21,6 +23,7 @@ function getUserInput() {
   return userInput;
 }
 
+//errors
 function resetSearchError() {
   document.getElementById('city-name').classList.remove('border-danger');
   document.getElementById('error').textContent = '';
@@ -31,64 +34,13 @@ function showSearchError(message) {
   document.getElementById('error').textContent = message;
 }
 
+/***** Getting weather data *****/
 function startGettingWeatherData(cityName) {
   const apiKey = '8364edf40aaaa47bca43e4b4901faf72';
   getFiveDayForecast(cityName, apiKey);
 }
 
-function properlyCapitalize(str) {
-  let copy = str;
-  copy = copy.toLowerCase();
-  return copy.charAt(0).toUpperCase() + copy.slice(1);
-}
-
-function getUVIndex(weatherObj, apiKey) {
-  const weatherData = weatherObj;
-  const uvIndexURL = getUVIndexURL(
-    weatherObj.coord.lat,
-    weatherObj.coord.lon,
-    apiKey);
-
-  fetch(uvIndexURL)
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(data){
-      weatherData.currentDay['uvIndex'] = data.value;
-
-      displayInformation(weatherData, weatherData.cityName);
-    });
-}
-
-function getUVIndexURL(lat, lon, apiKey) {
-  return 'http://api.openweathermap.org/data/2.5/uvi?'
-   + `lat=${lat}&lon=${lon}`
-   + `&appid=${apiKey}`;
-}
-
-function displayInformation(weatherObj, cityName) {
-  displayOverviewCard(weatherObj.currentDay , cityName);
-}
-
-function displayOverviewCard(currentDay, cityName) {
-  const displayDiv = document.getElementById('display-info');
-
-  displayDiv.innerHTML =
-    `<div class="card-body">
-      <h2 class="d-inline-block mr-3">${cityName} ${formatDate(currentDay.dt_txt)}</h2>
-      <img class="d-inline-block" src="http://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png" alt="${currentDay.weather[0].description}">
-      <p>Temperature: ${currentDay.main.temp} &#176;F</p>
-      <p>Humidity: ${currentDay.main.humidity}&#37;</p>
-      <p>Wind Speed: ${currentDay.wind.speed} MPH</p>
-      <p>UV Index: <span id="current-uv-index" class="bg-danger py-1 px-2 text-white rounded">${currentDay.uvIndex}</span></p>
-    </div>`;
-}
-
-function formatDate(date) {
-  const newDate = new Date(date);
-  return `(${newDate.getMonth() + 1}/${newDate.getDate()}/${newDate.getFullYear()})`;
-}
-
+//returns current day forecast, and next 5 days
 function getFiveDayForecast(cityName, apiKey) {
   const fiveDayForecastURL = getFiveDayForecastURL(cityName, apiKey);
   const currentDate = new Date();
@@ -135,6 +87,26 @@ function getFiveDayForecast(cityName, apiKey) {
     });
 }
 
+//get uv index of the current day
+function getUVIndex(weatherObj, apiKey) {
+  const weatherData = weatherObj;
+  const uvIndexURL = getUVIndexURL(
+    weatherObj.coord.lat,
+    weatherObj.coord.lon,
+    apiKey);
+
+  fetch(uvIndexURL)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      weatherData.currentDay['uvIndex'] = data.value;
+
+      displayInformation(weatherData, weatherData.cityName);
+    });
+}
+
+//getting URL queries
 function getFiveDayForecastURL(cityName, apiKey) {
   return 'http://api.openweathermap.org/data/2.5/forecast?'
    + `q=${cityName}`
@@ -142,6 +114,42 @@ function getFiveDayForecastURL(cityName, apiKey) {
    + `&appid=${apiKey}`;
 }
 
+function getUVIndexURL(lat, lon, apiKey) {
+  return 'http://api.openweathermap.org/data/2.5/uvi?'
+   + `lat=${lat}&lon=${lon}`
+   + `&appid=${apiKey}`;
+}
+
+/***** Display functions *****/
+function displayInformation(weatherObj, cityName) {
+  displayOverviewCard(weatherObj.currentDay , cityName);
+}
+
+function displayOverviewCard(currentDay, cityName) {
+  const displayDiv = document.getElementById('display-info');
+
+  displayDiv.innerHTML =
+    `<div class="card-body">
+      <h2 class="d-inline-block mr-3">${cityName} ${formatDate(currentDay.dt_txt)}</h2>
+      <img class="d-inline-block" src="http://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png" alt="${currentDay.weather[0].description}">
+      <p>Temperature: ${currentDay.main.temp} &#176;F</p>
+      <p>Humidity: ${currentDay.main.humidity}&#37;</p>
+      <p>Wind Speed: ${currentDay.wind.speed} MPH</p>
+      <p>UV Index: <span id="current-uv-index" class="bg-danger py-1 px-2 text-white rounded">${currentDay.uvIndex}</span></p>
+    </div>`;
+}
+
+/***** Formatting functions *****/
+function formatDate(date) {
+  const newDate = new Date(date);
+  return `(${newDate.getMonth() + 1}/${newDate.getDate()}/${newDate.getFullYear()})`;
+}
+
+function properlyCapitalize(str) {
+  let copy = str;
+  copy = copy.toLowerCase();
+  return copy.charAt(0).toUpperCase() + copy.slice(1);
+}
 //on page load
   //grab the search object from local storage
   //display the overview info from current (make another fetch)
